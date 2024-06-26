87be4700fc277871d610c02c866e60b01be3eaef
RasonYudha4 <Rasonyudha921@gmail.com>
2024-06-26 20:27:15 +0700

    Progressing on homepage and about us part

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -6,32 +6,12 @@ import ApplicationLogo from './ApplicationLogo';
 export default function NavBar({ user }) {
     return (
         <>
-            <header className=" md:bg-white mx-3 my-3 px-4 md:px-12 h-12 md:h-24 w-full fixed flex flex-1 justify-between items-center top-0 pb-3">
+            <header className=" bg-white px-4 md:px-12 h-12 md:h-24 w-full fixed flex flex-1 justify-between items-center top-0 pb-1">
                 <div className=''>
                     <Link href="/">
                         <ApplicationLogo className="h-10 block w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </Link>
                 </div>
-
-                {/* <nav>
-                    <button className='md:hidden'>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-[#5CE1E6]">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                        </svg>
-                    </button>
-                    <ul className='fixed left-0 right-0 p-4 transform translate-x-full min-h-screen space-y-4 md:space-y-0 md:translate-x-0 md:relative md:flex md:min-h-0 md:space-x-6 md:p-0'>
-                        <li>
-                            <a href="#about">About</a>
-                        </li>
-                        <li>
-                            <a href="#branch">Branch</a>
-                        </li>
-                        <li>
-                            <a href="#service">Services</a>
-                        </li>
-                    </ul>
-                </nav> */}
-
                 <div className="flex items-center ms-6">
                     <div className="ms-3 relative">
                         <Dropdown>
@@ -99,12 +79,6 @@ export default function NavBar({ user }) {
                                             className="rounded-md px-3 py-2 text-[#5CE1E6] text-lg font-extrabold ring-1 ring-transparent transition hover:text-[#22ABFA] focus:outline-none focus-visible:ring-[#38DDBF] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
                                             Log in
-                                        </Link>
-                                        <Link
-                                            href={route('register')}
-                                            className="rounded-md px-3 py-2 text-[#5CE1E6] text-lg font-extrabold ring-1 ring-transparent transition hover:text-[#22ABFA] focus:outline-none focus-visible:ring-[#38DDBF] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Register
                                         </Link>
                                     </>
                             )}
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -10,7 +10,14 @@ export default function Home({ auth, laravelVersion, phpVersion }) {
             <section className=" w-full h-full">
                 <img src={Background} alt="" className=''/>
             </section>
-            <section className=''>
+            <section className='p-4 w-full bg-[#cfe8d6]'>
+                <div className='mx-2 bg-white rounded-3xl'>
+                    <h2 className='font-black text-xl text-center pt-6 mb-6 md:text-4xl'>About Us</h2>
+                    <span className='bg-[#5CE1E6] h-5 w-8'></span>
+                    <p className='px-4 tracking-wide mt-4 text-xs text-center pb-8 md:text-xl md:px-20'>At SEA Salon, we're <span className='  font-black text-[#5CE1E6]'>more than just a salon</span>, we're a haven for self-care and a celebration of your unique beauty. We believe that <span className='  font-black text-[#5CE1E6]'>looking and feeling your best go hand-in-hand</span>, which is why we offer a <span className='  font-black text-[#5CE1E6]'>comprehensive range of services</span> in a warm and inviting atmosphere.</p>
+                </div>
+            </section>
+            <section className=' w-full h-screen bg-black'>
 
             </section>
         </>
