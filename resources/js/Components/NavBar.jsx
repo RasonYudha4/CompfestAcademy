19409a667a169d423edfbfbfa3a82b91a4108bd0
RasonYudha4 <Rasonyudha921@gmail.com>
2024-06-26 09:18:48 +0700

    Adding some responsive to the nav bar

--- resources/js/Components/NavBar.jsx
+++ resources/js/Components/NavBar.jsx
@@ -5,13 +5,32 @@ import ApplicationLogo from './ApplicationLogo';
 export default function NavBar({ user }) {
     return (
         <>
-            <nav className="mx-3 my-3 fixed flex flex-1 justify-between items-center top-0">
+            <header className=" bg-white mx-3 my-3 px-4 md:px-12 h-24 w-full fixed flex flex-1 justify-between items-center top-0">
                 <div className=''>
                     <Link href="/">
                         <ApplicationLogo className="h-10 block w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </Link>
                 </div>
-                <div className='flex flex-1 justify-end'>
+
+                <nav>
+                    <button className='md:hidden'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-[#5CE1E6]">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                    </button>
+                    <ul className='fixed left-0 right-0 p-4 transform translate-x-full min-h-screen space-y-4 md:space-y-0 md:translate-x-0 md:relative md:flex md:min-h-0 md:space-x-6 md:p-0'>
+                        <li>
+                            <a href="#about">About</a>
+                        </li>
+                        <li>
+                            <a href="#branch">Branch</a>
+                        </li>
+                        <li>
+                            <a href="#service">Services</a>
+                        </li>
+                    </ul>
+                </nav>
+                {/* <div className='flex flex-1 justify-end'>
                     {user ? (
                         <Link
                             href={route('dashboard')}
@@ -35,8 +54,8 @@ export default function NavBar({ user }) {
                             </Link>
                         </>
                     )}
-                </div>
-            </nav>
+                </div> */}
+            </header>
         </>
     )
 }
