b45c07ea45b2f52ed05a4166cb8343bac2b956ba
RasonYudha4 <Rasonyudha921@gmail.com>
2024-06-29 02:09:35 +0700

    Creating the reservation form ui

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -5,9 +5,9 @@ export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <NavBar user={user}/>
-            <main>{children}</main>
+            <main className="w-full sm:max-w-md mt-16 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">{children}</main>
         </div>
     );
 }
--- resources/js/Pages/Reservation.jsx
+++ resources/js/Pages/Reservation.jsx
@@ -1,14 +1,110 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { Head, Link, useForm } from '@inertiajs/react';
 
 export default function Reservation({ auth }) {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        name: '',
+        email: '',
+        phone: '',
+        password: '',
+        password_confirmation: '',
+    });
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route('register'));
+    };
+
     return (
         <>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.4.1/flowbite.min.js"></script>
             <Head title="Reserve" />
             <AuthenticatedLayout
                 user={auth.user}
             >
-                
+                <form onSubmit={submit}>
+                    <div>
+                        <InputLabel htmlFor="name" value="Name" />
+
+                        <TextInput
+                            id="name"
+                            name="name"
+                            value={data.name}
+                            className="mt-1 block w-full"
+                            autoComplete="name"
+                            isFocused={true}
+                            onChange={(e) => setData('name', e.target.value)}
+                            required
+                        />
+
+                        <InputError message={errors.name} className="mt-2" />
+                    </div>
+
+                    <div className="mt-4">
+                        <InputLabel htmlFor="phone" value="Phone Number" />
+
+                        <TextInput
+                            id="phone"
+                            type="number"
+                            name="phone"
+                            value={data.phone}
+                            className="mt-1 block w-full"
+                            autoComplete="username"
+                            onChange={(e) => setData('phone', e.target.value)}
+                            required
+                        />
+
+                        <InputError message={errors.phone} className="mt-2" />
+                    </div>
+
+                    <div className='mt-4'>
+                        <InputLabel htmlFor="phone" value="Phone Number" />
+
+                        <select name="services" id="service" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                            <option selected>Select service</option>
+                            <option value="Facial">Facial Treatments</option>
+                            <option value="Hair&Style">Haircut and Styling</option>
+                            <option value="Manicure">Manicure and Pedicure</option>
+                        </select>
+                    </div>
+
+                    <div class="mt-4">
+                    <InputLabel htmlFor="phone" value="Phone Number" />
+                        <div className='relative w-full'>
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                            </svg>
+                        </div>
+                        <input datepicker id="default-datepicker" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date"/>
+                    </div>
+                    </div>
+
+                    <div className='mt-4'>
+                        <InputLabel htmlFor="phone" value="Phone Number" />
+
+                        <select name="services" id="service" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                            <option selected>Select Time</option>
+                            <option value="09:00-10:00">09:00-10:00</option>
+                            <option value="10:00-11:00">10:00-11:00</option>
+                            <option value="11:00-12:00">11:00-12:00</option>
+                            <option value="13:00-14:00">13:00-14:00</option>
+                            <option value="14:00-15:00">14:00-15:00</option>
+                        </select>
+                    </div>
+
+                    <div className="flex items-center justify-end mt-4">
+
+                        <PrimaryButton className="ms-4" disabled={processing}>
+                            Submit
+                        </PrimaryButton>
+                    </div>
+                </form>
             </AuthenticatedLayout>
         </>
     )
